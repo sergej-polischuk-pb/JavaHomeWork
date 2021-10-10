9cd9263d7eb43cf4015e2823ccd25dddb462c4d9
Sergej Poliscuk <sergej.poliscuk@privatbank.ua>
2021-10-10 17:47:28 +0300

    init commit.

--- src/com/pb/poliscuk/HomeWork_1/HelloWorld.java
+++ src/com/pb/poliscuk/HomeWork_1/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.pb.poliscuk.HomeWork_1;
 
-public class HelloWorl {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("-== Hello World ==-");
     }
